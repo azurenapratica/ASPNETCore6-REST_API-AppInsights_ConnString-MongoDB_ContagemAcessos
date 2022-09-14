ae4c8d13ab765bbb5e099e2ce64d691c085c427c
Renato Groffe <renato.groffe@yahoo.com.br>
2022-09-14 02:52:21 -0300

    .NET 6 + ASP.NET Core + REST API Contagem de Acessos + Azure Application Insights (via Connection String) + MongoDB (persistência dos resultados)
    .NET 6 + ASP.NET Core + REST API Contagem de Acessos + Azure Application Insights (via Connection String) + MongoDB (persistência dos resultados)

---
+++ APIContagem/Data/ContagemDocument.cs
@@ -0,0 +1,13 @@
+using MongoDB.Bson;
+
+namespace APIContagem.Data;
+
+public class ContagemDocument
+{
+    public ObjectId _id { get; set; }
+    public int ValorAtual { get; set; }
+    public string? Producer { get; set; }
+    public string? Kernel { get; set; }
+    public string? Framework { get; set; }
+    public string? Mensagem { get; set; }
+}
